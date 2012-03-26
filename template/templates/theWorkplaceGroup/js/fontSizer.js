a4c01ee8b266bd0d89e9ba007d1033e73186e966
Matt Elliott <melliott@ccistudios.com>
2012-03-26 13:43:41 -0400

    fontResizer js created

--- template/templates/theWorkplaceGroup/js/fontSizer.js
+++ template/templates/theWorkplaceGroup/js/fontSizer.js
@@ -1,9 +1,24 @@
-$('.moduletable.fontResize').on('click', 'a.smaller', function() {
-	$('div#body p')
-		.addClass('smaller')
-});
-
-$('.moduletable.fontResize').on('click', 'a.bigger', function() {
-	$('div#body p')
-		.addClass('bigger')
+window.addEvent('domready', function() {
+	var resizer = $$('.moduletable.fontResize')[0],
+		minus = resizer.getElement('.smaller'),
+		plus = resizer.getElement('.bigger'),
+		content = $('content'),
+		current = parseInt(content.getStyle('font-size'), 10);
+		
+	console.log(current);
+	minus.addEvent('click', function(event) {
+		current--;
+		if (current < 9)
+			current = 9;
+		content.setStyle('font-size', current);
+		event.preventDefault();
+	});
+	
+	plus.addEvent('click', function(event) {
+		current++;
+		if (current > 16)
+			current = 16;
+		content.setStyle('font-size', current);
+		event.preventDefault();
+	});
 });
